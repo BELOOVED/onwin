c7f2a8baf5c707fa87ae240df08bc8781587a875
BELOOVED <aycahanim@outlook.com>
2022-11-20 11:44:03 +0300

    modified:   view/aviator.php

--- view/aviator.php
+++ view/aviator.php
@@ -1,1 +1,1 @@
-<iframe title="iframe" frameborder="0" scrolling="yes" style="width: 100%; height: 800px;" src="https://aviator-demo.spribegaming.com/?currency=USD&operator=demo&jurisdiction=CW&lang=EN&return_url=https%3A%2F%2Fspribe.co%2Fgames&user=86914&token=izH7HETTHBK7RGdalixrgvfA6lW6ntuk"></iframe>
+<iframe data-hj-allow-iframe="" src="https://demo.spribe.io/launch/aviator?lang=tr&amp;return_url=https%3A%2F%2Fwww.pin-up328.com" id="iframe" style="width: 100%; height: calc(100% - 35px) ;" allowfullscreen="true"></iframe>
