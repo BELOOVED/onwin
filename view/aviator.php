05b07d4b020cbc8d8ed773cd785af07d675572b8
BELOOVED <aycahanim@outlook.com>
2022-11-20 17:08:15 +0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	renamed:    view/spribe/aviator.php -> view/aviator.php
    	new file:   view/live-casino.php
    	deleted:    view/livecasino.php
    	modified:   view/livesports.php
    	modified:   view/modules/header.php
    	new file:   view/slot.php

---
+++ view/aviator.php
@@ -0,0 +1,1 @@
+<iframe data-hj-allow-iframe="" src="https://demo.spribe.io/launch/aviator?lang=tr&amp;return_url=https%3A%2F%2Fwww.pin-up328.com" id="iframe" style="width: 100%; height: calc(100% - 35px) ;" allowfullscreen="true"></iframe>
