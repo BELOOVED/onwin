ad979d4fb149d80b6753a17fc7480bb937fd019b
BELOOVED <aycahanim@outlook.com>
2022-11-25 18:13:18 +0300

    modified:   m/view/livesports.php 	modified:   m/view/modules/header.php

--- m/view/livesports.php
+++ m/view/livesports.php
@@ -2,5 +2,4 @@
 <center>
 
 
-<iframe style=" top:0; left:0; bottom:0; right:0; width:100%; height:100%; min-height:800px; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="https://sb1client-altenar.biahosted.com/?origin=https://sb1client-altenar.biahosted.com/static/&amp;token=&amp;lang=tr-tr&amp;walletcode=&amp;timezoneoffset=-180&amp;numformat=en-gb&amp;skinid=parsbet&amp;hasplacebetplatformerrorcallback=false#/"></iframe></center>
-</div>
+<iframe src="https://bet-onwin.erisgaming.com/prelive/day" id="iframe" style="width: 100%; height: calc(100% - 35px) ;" allowfullscreen="true" frameborder="0"></iframe></div>
