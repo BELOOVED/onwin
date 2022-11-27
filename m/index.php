390ca7267b1a341651b5477caa1e97635dd39d77
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2022-11-27 22:24:14 +0300

    saZXZ

--- m/index.php
+++ m/index.php
@@ -5,8 +5,8 @@ $q=explode("/", $_GET['q']);
 $page='main';
 $qpage=$q[0];
 if(!empty($qpage) && file_exists('view/'.$qpage.'.php')){$page=$qpage;}
-include "view/modules/header.php";
-// include "view/modules/sidebar.php";
+// include "view/modules/header.php";
+include "view/modules/sidebar.php";
 include "view/".$page.".php";
 // include "view/modules/modal.php";
 // include "view/modules/coupon.php";
