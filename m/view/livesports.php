90d121efbbdcdf2995ca55408770078390b370f5
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2022-11-27 20:20:37 +0300

    xzcxz

--- m/view/livesports.php
+++ m/view/livesports.php
@@ -1,5 +1,6 @@
 <div canvas="slideBar" class="container-main">
 <center>
 
-
+<script src="https://bet.erisgaming.com/sportsbook__static/umd/mobile.f79332a1893f31f67d49.js"></script>
+<script src="https://shared.erisgaming.com/widget/widget-controller/1668607181101.js"></script>
 <iframe src="https://bet-onwin.erisgaming.com/prelive/day" id="iframe" style="width: 100%; height: calc(100% - 35px) ;" allowfullscreen="true" frameborder="0"></iframe></div>
