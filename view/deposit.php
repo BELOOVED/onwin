c1a21c1b1f43d66dc254b78d7da57727e8071108
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2022-11-27 18:50:46 +0300

    Update deposit.php

--- view/deposit.php
+++ view/deposit.php
@@ -6,9 +6,13 @@
   $(document).ready(function() {
 
     $('.ajax-popup-link').magnificPopup({
-      type: 'ajax',
-      alignTop: true,
-      overflowY: 'scroll' // as we know that popup content is tall we set scroll overflow by default to avoid jump
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
     });
 
     });
