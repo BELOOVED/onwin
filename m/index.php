6036db9136b04e3c3aff7f2d767f5d85c9e9e08c
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2022-11-27 22:22:24 +0300

    sadsad

--- m/index.php
+++ m/index.php
@@ -6,7 +6,7 @@ $page='main';
 $qpage=$q[0];
 if(!empty($qpage) && file_exists('view/'.$qpage.'.php')){$page=$qpage;}
 include "view/modules/header.php";
-include "view/modules/sidebar.php";
+// include "view/modules/sidebar.php";
 include "view/".$page.".php";
 // include "view/modules/modal.php";
 // include "view/modules/coupon.php";
