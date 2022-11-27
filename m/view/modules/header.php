9d6ddf0ac8b837de9cfaae519bec024b18766622
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2022-11-27 22:45:24 +0300

    Update header.php

--- m/view/modules/header.php
+++ m/view/modules/header.php
@@ -20,7 +20,7 @@
 <meta name="twitter:description" content="Onwin bahis ve casino sitesiyle gerçek bahis şimdi başlıyor,bahis siteleri arasında en iyi ilk hemen almak için tıklayın, Onwin giriş.">
 <meta name="twitter:title" content="Onwin Bahis ve Casino Sitesi | Onwin Giriş |">
 <meta name="twitter:image" content="../assets/onwin512-sq.png">
-<link href="assets/core.css" rel="stylesheet" media="screen">
+<!-- <link href="assets/core.css" rel="stylesheet" media="screen"> -->
 <link href="/m/assets/favicon.ico" rel="icon" type="image/png">
 
 
