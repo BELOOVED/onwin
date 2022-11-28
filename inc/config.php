87e8961cb442372857ce5dceffea618dcf22b0e5
BELOOVED <aycahanim@outlook.com>
2022-11-28 14:15:43 +0300

    modified:   inc/config.php

--- inc/config.php
+++ inc/config.php
@@ -9,9 +9,9 @@ date_default_timezone_set('Europe/Istanbul');
 
 // Database Info
 const DB_HOST = "localhost";
-const DB_LOGIN = "u1832_a";
-const DB_PASS = "t6%xDn740";
-const DB_NAME = "u1832972_a";
+const DB_LOGIN = "u1851_on";
+const DB_PASS = "df1Jh094_";
+const DB_NAME = "u1851750_onwin";
 const SYSNAME = 'Onwin';
 const ENV = 'prod';
 
