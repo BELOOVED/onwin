84eee6bcb45e95b3e188169c8c7ee74bee872d41
BELOOVED <aycahanim@outlook.com>
2022-12-01 10:41:33 +0300

    modified:   m/view/livesports.php

--- m/view/livesports.php
+++ m/view/livesports.php
@@ -1,4 +1,5 @@
 <div canvas="slideBar" class="container-main">
 <center>
+<iframe src="https://bet-onwin.erisgaming.com/prelive/day" id="iframe" style="width: 100%; height: calc(100% - 35px) ;" allowfullscreen="true" frameborder="0"></iframe>
 
-<iframe src="https://bet-onwin.erisgaming.com/prelive/day" id="iframe" style="width: 100%; height: calc(100% - 35px) ;" allowfullscreen="true" frameborder="0"></iframe></div>
+    <script src="https://bet.erisgaming.com/sportsbook__static/umd/mobile.4f75d83788e682186764.js"></script> 
